2b4681be1b72396fca3acc9fd8d2631636bd8c41
ankita-acharekar <aacharekar@deqode.com>
2023-02-16 19:14:53 +0530

    initial commit

---
+++ src/main/java/com/deqode/grpc/DeqodeGrpcApplication.java
@@ -0,0 +1,26 @@
+package com.deqode.grpc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DeqodeGrpcApplication {
+	
+	final static Logger logger = LoggerFactory.getLogger(DeqodeGrpcApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeqodeGrpcApplication.class, args);
+	}
+
+	@GetMapping("/something")
+    public ResponseEntity<String> createLogs() {
+        logger.warn("Just checking");
+        return ResponseEntity.ok().body("All Ok");
+    }
+}
